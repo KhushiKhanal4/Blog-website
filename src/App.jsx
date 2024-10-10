d69c8918abfcffaf56ec96eb9128ed67fc8ecf35
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-10-10 22:12:06 +0545

    add env

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function App() {
-  return (  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    
+    <>  
     <div className='font-bold text-3xl text-blue-600 text-center m-9'>Hiii</div>
+
+    
+    </>
   ) 
 }
 
