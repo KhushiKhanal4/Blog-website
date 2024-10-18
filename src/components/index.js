80bc6a4acaeeb81f7dfa12fa5a6cb604c8ff9ce9
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-10-18 21:53:50 +0545

    create login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutButton from "./Header/LogoutButton";
-
+import Button from "./Button";
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutButton
+    LogoutButton,
+    Button
 }
