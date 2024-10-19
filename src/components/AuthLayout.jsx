cc60b0b5c7ca531ac6161853d70381f5191968f8
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-10-19 14:13:21 +0545

    create protection mechanism

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,27 @@
+//protection management
+
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+export default function Protected({ children, authentication = true }) {
+    const [loader, setLoader] = useState(true);
+    const navigate = useNavigate()
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        if (authentication && authStatus !== authentication) {
+            navigate("/login")
+        }
+        else if (!authentication && authStatus !== authentication) {
+            navigate = ( "/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+
+    return (
+        <div>AuthLayout</div>
+    )
+}
+
